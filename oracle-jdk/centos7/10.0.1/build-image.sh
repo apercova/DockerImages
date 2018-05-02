87d56e691e76b5459f108878be1137e512be24a2
apercova <alonsopc91@gmail.com>
2018-05-01 19:33:38 -0500

    Centos7 and Ubuntu18.04 JDK images

---
+++ oracle-jdk/centos7/10.0.1/build-image.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "#########################################################################"
+
+IMG_NAME="apercova/centos7-jdk"
+FILE_PATTERN="jdk-10*linux*.tar.gz"
+FILENAME_REGEXP="(jdk-)(10\.[0-9]{1,2}\.[0-9]{1,3})(.*\.tar\.gz)"
+DEF_VERSION="10.generic"
+
+
+echo "Getting image version ..."
+echo ""
+if [ `ls -al $FILE_PATTERN 2>/dev/null | wc -l` -gt 1 ]; then
+  echo "ERROR: Multiple versions of jdk tarball file.";
+  echo "ERROR: Only one tarball file is allowed into this directory!";
+  exit 1;
+else
+  FILENAME=$(ls $FILE_PATTERN 2>/dev/null)
+  echo $FILENAME
+  echo $FILENAME_REGEXP
+  if [[ $FILENAME =~ $FILENAME_REGEXP ]]; then
+    VERSION=${BASH_REMATCH[2]}
+  else
+    VERSION="$DEF_VERSION"
+  fi;
+fi;
+IMG_NAME="$IMG_NAME:$VERSION"
+docker info
+echo ""
+echo "Building image $IMG_NAME"
+echo ""
+docker build -t $IMG_NAME .
+echo ""
+echo "#########################################################################"
+
+
---
+++ oracle-jdk/ubuntu18.04/7u80/build-image.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "#########################################################################"
+
+IMG_NAME="apercova/ubuntu18.04-jdk"
+FILE_PATTERN="jdk-7*linux*.tar.gz"
+FILENAME_REGEXP="(jdk-)(7\w+)(.*\.tar\.gz)"
+DEF_VERSION="7.generic"
+
+
+echo "Getting image version ..."
+echo ""
+if [ `ls -al $FILE_PATTERN 2>/dev/null | wc -l` -gt 1 ]; then
+  echo "ERROR: Multiple versions of jdk tarball file.";
+  echo "ERROR: Only one tarball file is allowed into this directory!";
+  exit 1;
+else
+  FILENAME=$(ls $FILE_PATTERN 2>/dev/null)
+  
+  if [[ $FILENAME =~ $FILENAME_REGEXP ]]; then
+    VERSION=${BASH_REMATCH[2]}
+  else
+    VERSION="$DEF_VERSION"
+  fi;
+fi;
+IMG_NAME="$IMG_NAME:$VERSION"
+docker info
+echo ""
+echo "Building image $IMG_NAME"
+echo ""
+docker build -t $IMG_NAME .
+echo ""
+echo "#########################################################################"
+
+
